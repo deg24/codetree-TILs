84b5f486188dfce8cea4690f60a3468c89a3f4e8
12243935 <77370202+deg24@users.noreply.github.com>
2024-03-26 21:16:02 +0900

    Title: 소수점 반올림하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240326/소수점 반올림하기/rounding-decimal-points.cpp
+++ 240326/소수점 반올림하기/rounding-decimal-points.cpp
@@ -3,11 +3,13 @@ using namespace std;
 
 int main() {
     // 여기에 코드를 작성해주세요.
+    cout << fixed;
+
     double a = 25.352;
 
     cout.precision(1);
 
-    cout << a;
+    cout << a << endl;
 
     return 0;
 }
