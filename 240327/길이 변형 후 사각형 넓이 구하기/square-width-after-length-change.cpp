3d22f8cefe2ce149b70f76fe2f6b47d01818072b
12243935 <77370202+deg24@users.noreply.github.com>
2024-03-27 21:33:44 +0900

    Title: 길이 변형 후 사각형 넓이 구하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240327/길이 변형 후 사각형 넓이 구하기/square-width-after-length-change.cpp
+++ 240327/길이 변형 후 사각형 넓이 구하기/square-width-after-length-change.cpp
@@ -4,6 +4,8 @@ using namespace std;
 int main() {
     int wid, leng;
     cin >> wid >> leng;
+    wid += 8;
+    leng *= 3;
     ccout << wid << endl << leng << endl << wid * leng;
     return 0;
 }
