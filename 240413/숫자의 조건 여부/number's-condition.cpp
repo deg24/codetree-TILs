d263ba04694054373eef9a0db0335ca8e8eac06b
12243935 <77370202+deg24@users.noreply.github.com>
2024-04-13 22:43:24 +0900

    Title: 숫자의 조건 여부, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240413/숫자의 조건 여부/number's-condition.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n{0};
+
+    cin >> n;
+
+    if(a>=113){
+        cout << 1;
+    }
+    else {
+        cout << 0;
+    }
+
+    return 0;
+}
