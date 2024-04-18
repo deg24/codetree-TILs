9b5d8415ffd2627a0ab5ccdcb24dbe2400b34f50
12243935 <77370202+deg24@users.noreply.github.com>
2024-04-18 13:01:17 +0900

    Title: 살 수 있는 물건 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240418/살 수 있는 물건 2/things-able-to-buy-2.cpp
+++ 240418/살 수 있는 물건 2/things-able-to-buy-2.cpp
@@ -4,9 +4,9 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    if(n>=3000)[
+    if(n>=3000){}
         cout << "book";
-    ]else if(n>=1000){
+    }else if(n>=1000){
         cout << "mask";
     }else if(n>=500){
         cout << "pen";
