6f318ba28d8a5cc71a2d948901f8f46144921faa
12243935 <77370202+deg24@users.noreply.github.com>
2024-05-23 12:24:51 +0900

    Title: 좀 더 어려운 수학 점수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240523/좀 더 어려운 수학 점수/math-scores-are-more-difficult.cpp
+++ 240523/좀 더 어려운 수학 점수/math-scores-are-more-difficult.cpp
@@ -6,7 +6,9 @@ int main() {
 	cin >> a >> b >> c >> d;
 	if (a > b)cout << "A";
 	else if (a < b)cout << "B";
-	else if (c > d)cout << "A";
-	else cout << "B";
+	else if (a == b) {
+		if (c > d)cout << "A";
+		else cout << "B";
+	}
 	return 0;
 }
