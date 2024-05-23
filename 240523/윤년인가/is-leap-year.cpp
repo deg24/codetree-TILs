b294ee52f8db584e65fee85e66db38efedf33342
12243935 <77370202+deg24@users.noreply.github.com>
2024-05-23 13:22:51 +0900

    Title: 윤년인가, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240523/윤년인가/is-leap-year.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a;
+	cin >> a;
+	if (a % 4 == 0) {
+		if (a % 400 == 0)cout << "true";
+		else if (a % 100 == 0)cout << "false";
+		else cout << "true";
+	}
+	else cout << "false";
+	return 0;
+}
