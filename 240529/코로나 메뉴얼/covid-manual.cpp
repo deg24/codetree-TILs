0a75f046c4d9af32e1324a8a1a5f4830aacb2fd1
12243935 <77370202+deg24@users.noreply.github.com>
2024-05-29 15:18:37 +0900

    Title: 코로나 메뉴얼, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240529/코로나 메뉴얼/covid-manual.cpp
+++ 240529/코로나 메뉴얼/covid-manual.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-	char a, b, c, A;
+	char a, b, c, A{ 0 };
 	int aa, bb, cc;
 	cin >> a >> aa >> b >> bb >> c >> cc;
 	if (a == 'Y' && aa >= 37) A++;
@@ -10,6 +10,6 @@ int main() {
 	if (c == 'Y' && cc >= 37) A++;
 	if (A >= 2) cout << "E";
 	else cout << "N";
-
+	
 	return 0;
 }
