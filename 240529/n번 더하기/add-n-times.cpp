fddf48e7384b167812849df442e56a67d0772cc8
12243935 <77370202+deg24@users.noreply.github.com>
2024-05-29 18:27:43 +0900

    Title: n번 더하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240529/n번 더하기/add-n-times.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	for (int i{ 0 }; i < b; i++) {
+		a += b;
+		cout << a << "\n";
+	}
+	return 0;
+}
