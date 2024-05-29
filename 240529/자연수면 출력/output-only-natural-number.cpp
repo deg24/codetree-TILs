2d4fb2d1ba497a097649644175e7f25a0afb758c
12243935 <77370202+deg24@users.noreply.github.com>
2024-05-29 18:33:33 +0900

    Title: 자연수면 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240529/자연수면 출력/output-only-natural-number.cpp
+++ 240529/자연수면 출력/output-only-natural-number.cpp
@@ -4,15 +4,11 @@ using namespace std;
 int main() {
 	int a, b;
 	cin >> a >> b;
-	if (a > b) {
-		for (int i{ a }; i >= b; i--) {
-			cout << i << " ";
+	if (a > 0) {
+		for (int i{ 0 }; i < b; i++) {
+			cout << a;
 		}
 	}
-	else if(a<b){
-		for (int i{ b }; i >= a; i--) {
-			cout << i << " ";
-		}
-	}
+	else cout << 0;
 	return 0;
 }
