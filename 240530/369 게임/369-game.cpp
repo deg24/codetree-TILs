96934a6ff0214776d99ab7d80c4bec2e3d1c7d51
12243935 <77370202+deg24@users.noreply.github.com>
2024-05-30 12:30:13 +0900

    Title: 369 게임, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240530/369 게임/369-game.cpp
+++ 240530/369 게임/369-game.cpp
@@ -5,8 +5,7 @@ int main() {
 	int a;
 	cin >> a;
 	for (int i{ 1 }; i <= a; i++) {
-		if (i % 3 == 0) cout << 0 << " ";
-		else if (i % 30 >= 0 && i % 30 <= 9 && i>=10) cout << 0 << " ";
+		if (i % 3 == 0 || i / 10 == 3 || i / 10 == 6 || i / 10 == 9 || i % 10 == 3 || i % 10 == 6 || i % 10 == 9) cout << 0 << " ";
 		else cout << i << " ";
 	}
 	return 0;
