b30414dd7cbbf1a71431b3279c622918b870587e
12243935 <77370202+deg24@users.noreply.github.com>
2024-06-09 21:02:04 +0900

    Title: 몫 출력하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240609/몫 출력하기/print-share.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n=1, cnt = 0;
+	while (cnt <= 3) {
+		cin >> n;
+		if (n % 2 == 1) {}
+		else {
+			cout << n / 2 << "\n";
+			cnt++;
+		}
+	}
+	return 0;
+}
