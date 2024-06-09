e2763717aafa0a7f812314d57b27f4828ffe6921
12243935 <77370202+deg24@users.noreply.github.com>
2024-06-09 20:29:02 +0900

    Title: 특정 조건의 온전수 구하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240609/특정 조건의 온전수 구하기/print-onjeonsu.cpp
+++ 240609/특정 조건의 온전수 구하기/print-onjeonsu.cpp
@@ -7,9 +7,11 @@ int main() {
 	for (int i = 1; i <= a; i++) {
 		if (i % 2 == 1) {
 			if (i % 10 != 5) {
-				if (i % 9 == 0 && i % 3 != 0) {
-					cout << i << " ";
-					continue;
+				if (i % 3 == 0) {
+					if (i % 9 == 0) {
+						cout << i << " ";
+						continue;
+					}
 				}
 				cout << i << " ";
 			}
