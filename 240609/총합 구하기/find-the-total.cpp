973abda84c7ec1d235a4fe9a000dcf6f0b6b68c7
12243935 <77370202+deg24@users.noreply.github.com>
2024-06-09 19:58:31 +0900

    Title: 총합 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240609/총합 구하기/find-the-total.cpp
+++ 240609/총합 구하기/find-the-total.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
 	int a, b, s = 0;
 	cin >> a >> b;
-	for (int i = a; i < b; i++) {
+	for (int i = a; i <= b; i++) {
 		if (i % 6 == 0 && i % 8 != 0) {
 			s += i;
 		}
