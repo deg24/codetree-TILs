614c746c7ea97e941d644129a822c76240db27a9
12243935 <77370202+deg24@users.noreply.github.com>
2024-06-24 10:54:04 +0900

    Title: 3n + 1 수열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240624/3n + 1 수열/3n-plus-1-sequence.cpp
+++ 240624/3n + 1 수열/3n-plus-1-sequence.cpp
@@ -5,6 +5,9 @@ int main() {
 	int n, cnt = 0;
 	cin >> n;
 	while (1) {
+		if (n == 1) {
+			break;
+		}
 		if (n % 2 == 0) {
 			n /= 2;
 			cnt++;
@@ -12,9 +15,6 @@ int main() {
 		else if (n % 2 == 1) {
 			n = n * 3 + 1;
 			cnt++;
-		}
-		if (n == 1) {
-			break;
 		}
 	}
 	cout << cnt;
