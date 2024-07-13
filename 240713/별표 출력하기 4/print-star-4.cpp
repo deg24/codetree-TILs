4800ad03accc81e9390cb909415788fadf9b35c8
12243935 <77370202+deg24@users.noreply.github.com>
2024-07-13 23:40:34 +0900

    Title: 별표 출력하기 4, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240713/별표 출력하기 4/print-star-4.cpp
+++ 240713/별표 출력하기 4/print-star-4.cpp
@@ -11,7 +11,7 @@ int main() {
 		cout << "\n";
 	}
 	for (int i = 0; i < n - 1; i++) {
-		for (int j = 0; j < n-1+i; j++) {
+		for (int j = 0; j < 2+i; j++) {
 			cout << "* ";
 		}
 		cout << "\n";
