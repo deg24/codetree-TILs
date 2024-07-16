6314f227f0dcb573ae2c69851fa691e63fe8a2cd
12243935 <77370202+deg24@users.noreply.github.com>
2024-07-16 16:43:35 +0900

    Title: 1-9 정사각형, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240716/1-9 정사각형/1-9-square.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+
+	for (int i = 1, k = 0; k < n; k++) {
+		for (int j = 0; j < n; j++) {
+			cout << i;
+			if (i == 9) i = 0;
+			i += 1;
+		}
+		cout << "\n";
+	}
+	
+	return 0;
+}
