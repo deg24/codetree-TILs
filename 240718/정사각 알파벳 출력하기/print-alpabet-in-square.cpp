36f0f6a1c23326994d160216bc7c110fee628650
12243935 <77370202+deg24@users.noreply.github.com>
2024-07-18 20:21:45 +0900

    Title: 정사각 알파벳 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240718/정사각 알파벳 출력하기/print-alpabet-in-square.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n, a = 65;
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++, a++) {
+			cout << (char)a;
+		}
+		cout << "\n";
+	}
+	
+	return 0;
+}
