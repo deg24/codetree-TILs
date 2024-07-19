68e9c776ed55962e6e00c7f0dbdea89110481900
12243935 <77370202+deg24@users.noreply.github.com>
2024-07-19 15:21:33 +0900

    Title: 배열의 특정 위치 찾기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240719/배열의 특정 위치 찾기/find-specific-location-fo-array.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a[10], n1 = 0, cnt = 0;
+	double n2 = 0;
+
+	cout << fixed;
+	cout.precision(1);
+
+	for (int i = 1; i <= 10; i++) {
+		cin >> a[i - 1];
+		if (i % 2 == 0) {
+			n1 += a[i - 1];
+		}
+		if (i % 3 == 0) {
+			n2 += a[i - 1];
+			cnt++;
+		}
+	}
+	
+	cout << n1 << " " << n2 / cnt;
+
+
+	return 0;
+}
