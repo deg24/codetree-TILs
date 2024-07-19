a04e571be532fef7272e6b751270303af4e7a275
12243935 <77370202+deg24@users.noreply.github.com>
2024-07-19 21:14:41 +0900

    Title: 배열 놀이, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240719/배열 놀이/play-with-array.cpp
+++ 240719/배열 놀이/play-with-array.cpp
@@ -2,40 +2,39 @@
 using namespace std;
 
 int main() {
-	int n, q, a[100] = { 0, }, b, c, d;
+	int n, q, arr[100] = { 0, }, k, a, b, s, e;
 	cin >> n >> q;
 
 	for (int i = 0; i < n; i++) {
-		cin >> a[i];
+		cin >> arr[i];
 	}
 
 	for (int i = 0; i < q; i++) {
-		cin >> b;
-		if (b == 1) {
-			cin >> c;
-			cout << a[c - 1] << "\n";
+		cin >> k;
+		if (k == 1) {
+			cin >> a;
+			cout << arr[a - 1] << "\n";
 		}
-		if (b == 2) {
-			cin >> c;
+		if (k == 2) {
+			cin >> b;
 			for (int j = 0; j < n; j++) {
-				if (c == a[j]) {
+				if (b == arr[j]) {
 					cout << j + 1 << "\n";
 					continue;
 				}
-				if (j == n - 1 && c != a[j]) {
+				if (j == n - 1 && b != arr[j]) {
 					cout << 0 << "\n";
-					continue;
 				}
 			}
 		}
-		if (b == 3) {
-			cin >> c >> d;
-			for (int j = c; j <= d;j++) {
-				if (j == d) {
-					cout << a[j - 1] << "\n";
+		if (k == 3) {
+			cin >> s >> e;
+			for (int j = e; j <= s;j++) {
+				if (j == s) {
+					cout << arr[j - 1] << "\n";
 				}
 				else {
-					cout << a[j - 1] << " ";
+					cout << arr[j - 1] << " ";
 				}
 			}
 		}
