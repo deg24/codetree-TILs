fa61d519bcf71e7f2bf6761553c0967bd991d77a
12243935 <77370202+deg24@users.noreply.github.com>
2024-07-19 15:27:38 +0900

    Title: 특정 위치의 문자 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240719/특정 위치의 문자 2/char-in-specific-location-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	char a[10];
+
+	for (int i = 0; i < 10; i++) {
+		cin >> a[i];
+	}
+	cout << a[1] << " ";
+	cout << a[4] << " ";
+	cout << a[7] << " ";
+
+	return 0;
+}
