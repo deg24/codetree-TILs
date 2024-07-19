5ced35e5aa4753de4dfb8488f3693cdabb722002
12243935 <77370202+deg24@users.noreply.github.com>
2024-07-19 15:42:43 +0900

    Title: 배열에서 원소의 특징, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240719/배열에서 원소의 특징/characteristics-of-elements-in-an-array.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a[10];
+
+	for (int i = 0; i < 10; i++) {
+		cin >> a[i];
+		if (a[i] % 3 == 0) {
+			cout << a[i - 1];
+			break;
+		}
+	}
+
+	return 0;
+}
