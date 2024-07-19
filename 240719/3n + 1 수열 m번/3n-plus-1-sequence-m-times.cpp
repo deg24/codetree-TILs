c11003433235f9bd0a4f64f9c983258ba21f9607
12243935 <77370202+deg24@users.noreply.github.com>
2024-07-19 14:52:15 +0900

    Title: 3n + 1 수열 m번, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240719/3n + 1 수열 m번/3n-plus-1-sequence-m-times.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n, a;
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		int cnt = 0;
+		cin >> a;
+		for (int j = 0; a != 1; cnt++) {
+			if (a % 2 == 0) a /= 2;
+			else a = a * 3 + 1;
+		}
+		cout << cnt;
+	}
+	
+	return 0;
+}
