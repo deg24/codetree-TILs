91970ed0e9b7056828c2081ba1da52ba62579106
12243935 <77370202+deg24@users.noreply.github.com>
2024-07-19 20:34:05 +0900

    Title: 특정 위치의 문자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240719/특정 위치의 문자/char-in-specific-location.cpp
+++ 240719/특정 위치의 문자/char-in-specific-location.cpp
@@ -4,16 +4,17 @@ using namespace std;
 int main() {
 	char c[6] = { 'L', 'E', 'B', 'R', 'O', 'S' }, ch;
 	cin >> ch;
+	int n = 0;
 	
 	for (int i = 0; i < 6; i++) {
 		if (ch == c[i]) {
 			cout << i;
+			n++;
 			break;
 		}
-		else if (i == 6) {
-			cout << "None";
-		}
 	}
+
+	if (n == 0) cout << "None";
 
 	return 0;
 }
