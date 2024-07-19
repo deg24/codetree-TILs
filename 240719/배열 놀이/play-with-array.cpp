a4b5914cc8a8d1f3e4129712fddfed88b939d92a
12243935 <77370202+deg24@users.noreply.github.com>
2024-07-19 20:59:39 +0900

    Title: 배열 놀이, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240719/배열 놀이/play-with-array.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n, q, a[100] = { 0, }, b, c, d;
+	cin >> n >> q;
+
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+
+	for (int i = 0; i < q; i++) {
+		cin >> b;
+		if (b == 1) {
+			cin >> c;
+			cout << a[c - 1] << "\n";
+		}
+		if (b == 2) {
+			cin >> c;
+			for (int j = 0; j < n; j++) {
+				if (c == a[j]) {
+					cout << j + 1 << "\n";
+					continue;
+				}
+			}
+		}
+		if (b == 3) {
+			cin >> c >> d;
+			for (int j = c; j <= d;j++) {
+				if (j == d) {
+					cout << a[j - 1] << "\n";
+				}
+				else {
+					cout << a[j - 1] << " ";
+				}
+			}
+		}
+	}
+
+	return 0;
+}
