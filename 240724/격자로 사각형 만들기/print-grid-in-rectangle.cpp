bc94605f263fe6aafddb10baad31025d0ba50e74
12243935 <77370202+deg24@users.noreply.github.com>
2024-07-24 18:34:23 +0900

    Title: 격자로 사각형 만들기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240724/격자로 사각형 만들기/print-grid-in-rectangle.cpp
+++ 240724/격자로 사각형 만들기/print-grid-in-rectangle.cpp
@@ -9,7 +9,7 @@ int main() {
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
 			if (i == 0 || j == 0) {
-				a[i][j] = 0;
+				a[i][j] = 1;
 			}
 			else {
 				a[i][j] = a[i - 1][j - 1] + a[i][j - 1] + a[i - 1][j];
