4bc1b7446188a92ee51652a5ad52bbdd6148402b
12243935 <77370202+deg24@users.noreply.github.com>
2024-08-02 21:54:19 +0900

    Title: 문자 개수 세기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240802/문자 개수 세기/count-char.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string s;
+	getline(cin, s);
+	char c;
+	cin >> c;
+	int cnt = 0;
+	for (int i = 0; i < s.size(); i++) {
+		if (cnt == c) cnt++;
+	}
+	cout << cnt;
+	return 0;
+}
