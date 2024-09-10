2cde3892c17641b3a17c0a0b696ddb97caea8b5a
12243935 <77370202+deg24@users.noreply.github.com>
2024-09-10 19:02:46 +0900

    Title: 문자열 범위 출력하기 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240910/문자열 범위 출력하기 2/print-string-in-range-2.cpp
+++ 240910/문자열 범위 출력하기 2/print-string-in-range-2.cpp
@@ -6,8 +6,15 @@ int main() {
 	string s;
 	int a;
 	cin >> s >> a;
-	for (int i = 1, j = s.length(); i <= a; i++) {
-		cout << s[j - i];
+	if (s.length() <= a) {
+		for (int i = 1, j = s.length(); i <= a; i++) {
+			cout << s[j - i];
+		}
+	}
+	else {
+		for (int i = 1, j = s.length(); i <= s.length; i++) {
+			cout << s[j - i];
+		}
 	}
 
 	return 0;
