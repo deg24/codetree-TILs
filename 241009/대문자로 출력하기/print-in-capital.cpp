45f8c87942be836ec837c5228c4bd694e6c942a8
12243935 <77370202+deg24@users.noreply.github.com>
2024-10-09 14:55:19 +0900

    Title: 대문자로 출력하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241009/대문자로 출력하기/print-in-capital.cpp
+++ 241009/대문자로 출력하기/print-in-capital.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 
 int main() {
-    char ch[100];
+    char ch[100] = { '\0', };
     cin >> ch;
     int temp;
     char a[100] = { '\0', };
     int cnt = 0;
     for (int i = 0, j = 0; i < 100; i++) {
         if (ch[i] >= 'a' && ch[i] <= 'z') {
-            temp = (int)ch[i] + (int)'A' - (int)'a';
+            temp = (int)ch[i] + 32;
             a[j] = (char)temp;
             j++;
             cnt++;
